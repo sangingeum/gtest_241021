1abce7414702ed21d7180c7abd7b93462c1e8f82
Chansik Yun <chansigi@gmail.com>
2024-10-21 05:26:35 +0000

    .

--- 5_전역픽스쳐.cpp
+++ 5_전역픽스쳐.cpp
@@ -27,17 +27,38 @@ public:
     }
 };
 
+class MyEnvironment2 : public testing::Environment {
+public:
+    void SetUp() override
+    {
+        std::cout << "MyEnvironment2.SetUp()" << std::endl;
+    }
+
+    void TearDown() override
+    {
+        std::cout << "MyEnvironment2.TearDown()" << std::endl;
+    }
+};
+
 // 2) 설치방법 2가지
 //  => main 함수를 직접 정의할 때
+//  => 여러 개의 전역 픽스쳐를 등록하는 경우, 반드시 main 함수를 직접 정의해서 사용하는 것이 안전합니다.
 int main(int argc, char** argv)
 {
     testing::InitGoogleTest(&argc, argv);
 
     testing::AddGlobalTestEnvironment(new MyEnvironment);
+    testing::AddGlobalTestEnvironment(new MyEnvironment2);
+    // 객체를 생성할 때는 반드시 new를 통해서 생성해야 합니다.
 
     return RUN_ALL_TESTS();
 }
 
 //  => main 함수를 라이브러리에 포함시켰을 때
 //   : 전역 변수의 초기화가 main 함수 이전에 수행되는 특징을 이용합니다.
-// testing::Environment* my_env = testing::AddGlobalTestEnvironment(new MyEnvironment);
+//    - C++ 표준에서 전역 변수의 초기화가 각 파일의 단위에서 어떤 순서로 초기화될 지 미정의 사항입니다.
+// - test1.cpp
+// testing::Environment* my_env1 = testing::AddGlobalTestEnvironment(new MyEnvironment);
+
+// - test2.cpp
+// testing::Environment* my_env2 = testing::AddGlobalTestEnvironment(new MyEnvironment);
