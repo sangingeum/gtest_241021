aa5e2c773cb7c7f00210be0a45b48b47bec03fb1
Chansik Yun <chansigi@gmail.com>
2024-10-21 07:08:51 +0000

    .

--- 7_GoogleTest.cpp
+++ 7_GoogleTest.cpp
@@ -86,3 +86,11 @@ TEST(SampleTest, foo)
 //  $ ./a.out --gtest_output=xml
 // => 테스트의 결과를 JSON 형식으로 export 할 수 있습니다. - Google Test(1.10 이후)
 //  $ ./a.out --gtest_output=json
+
+// 5. 테스트 결과 포맷에 추가적인 정보를 기록할 수 있습니다.
+// => RecordProperty("key", "value")
+TEST(ImageTest, ResizeImage)
+{
+    RecordProperty("cpu", "1.5");
+    RecordProperty("mem", "12313223");
+}
