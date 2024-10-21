bc1208499a9f2ba63ff9cb9d62a0d5732a075fb4
Chansik Yun <chansigi@gmail.com>
2024-10-21 04:08:08 +0000

    .

--- 3_테스트픽스쳐3.cpp
+++ 3_테스트픽스쳐3.cpp
@@ -19,12 +19,18 @@ public:
 // => xUnit Test Framework이 제공하는 기능입니다.
 // 방법: 여러 테스트케이스 안에서 같은 테스트 픽스쳐 설치의 코드를 암묵적으로 호출되는 약속된 함수를 통해서 처리합니다.
 //  - 명시적인 테스트 스위트 클래스가 제공되어야 합니다.
+//  * 장점: 테스트 코드 중복을 제거하고, 불필요한 준비 과정을 테스트 케이스안에서 제거할 수 있습니다.
+//  * 단점: 픽스쳐 설치의 과정이 테스트 케이스 외부에 존재하기 때문에
+//         테스트케이스 만으로 인과관계를 분석하기 어려울 수 있습니다.
 
 class CalcTest : public testing::Test {
 protected:
+    Calc* calc;
+
     void SetUp() override
     {
         std::cout << "SetUp()" << std::endl;
+        calc = new Calc;
     }
 };
 
@@ -33,8 +39,6 @@ protected:
 TEST_F(CalcTest, PressPlus_TwoPlusTwo_DisplaysFour)
 {
     SPEC("2 더하기 2를 하였을 때, 4의 결과가 나오는지 여부를 검증합니다.");
-    // Arrange
-    Calc* calc = new Calc;
 
     // Act
     calc->Enter(2);
@@ -49,8 +53,6 @@ TEST_F(CalcTest, PressPlus_TwoPlusTwo_DisplaysFour)
 
 TEST_F(CalcTest, PressMinus)
 {
-    Calc* calc = new Calc;
-
     calc->Enter(10);
     calc->PressMinus();
     calc->Enter(5);
