2ed9ca319e2df0caedae1a5118784265e105db63
Chansik Yun <chansigi@gmail.com>
2024-10-21 04:13:49 +0000

    .

--- 3_테스트픽스쳐3.cpp
+++ 3_테스트픽스쳐3.cpp
@@ -1,7 +1,10 @@
 // 3_테스트픽스쳐3.cpp
+#include <iostream>
+
 class Calc {
 public:
     // Calc(int n) { } // !!!
+    ~Calc() { std::cout << "객체 파괴" << std::endl; }
 
     double Display() { return 0.0; }
 
@@ -15,9 +18,9 @@ public:
 #include <gtest/gtest.h>
 
 // 2. Test Fixture를 설치하는 방법
-// 3) Implicit Set up(암묵적 설치)
+// 3) Implicit Set up/Tear down(암묵적 설치/해체)
 // => xUnit Test Framework이 제공하는 기능입니다.
-// 방법: 여러 테스트케이스 안에서 같은 테스트 픽스쳐 설치의 코드를 암묵적으로 호출되는 약속된 함수를 통해서 처리합니다.
+// 방법: 여러 테스트케이스 안에서 같은 테스트 픽스쳐 설치/해체의 코드를 암묵적으로 호출되는 약속된 함수를 통해서 처리합니다.
 //  - 명시적인 테스트 스위트 클래스가 제공되어야 합니다.
 //  * 장점: 테스트 코드 중복을 제거하고, 불필요한 준비 과정을 테스트 케이스안에서 제거할 수 있습니다.
 //  * 단점: 픽스쳐 설치의 과정이 테스트 케이스 외부에 존재하기 때문에
@@ -31,6 +34,12 @@ protected:
     {
         std::cout << "SetUp()" << std::endl;
         calc = new Calc;
+    }
+
+    void TearDown() override
+    {
+        std::cout << "TearDown()" << std::endl;
+        delete calc; // !!!
     }
 };
 
@@ -49,6 +58,9 @@ TEST_F(CalcTest, PressPlus_TwoPlusTwo_DisplaysFour)
     // Assert
     // xUnit Test Framework은 다양한 단언문을 제공하고 있습니다.
     ASSERT_EQ(calc->Display(), 4) << "2 + 2 하였을 때";
+
+    // 단언문은 실패할 경우, 이후의 코드를 더 이상 수행하지 않습니다.
+    // delete calc;
 }
 
 TEST_F(CalcTest, PressMinus)
@@ -59,4 +71,6 @@ TEST_F(CalcTest, PressMinus)
     calc->PressEquals();
 
     ASSERT_EQ(calc->Display(), 5) << "10 - 5를 하였을 때";
+
+    // delete calc;
 }
