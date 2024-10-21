5188f17a87e6ca4db06243b3eeab180866dc4770
Chansik Yun <chansigi@gmail.com>
2024-10-21 01:34:02 +0000

    .

--- 1_시작.cpp
+++ 1_시작.cpp
@@ -2,9 +2,13 @@
 #include <gtest/gtest.h>
 
 // Google Test의 main은 아래 형태로 제공되어야 합니다.
+// - Google Test는 다양한 환경에서 동작하는 main을 이미 제공하고 있습니다.
+// - 라이브러리에 main을 포함시키면, 매번 main을 작성할 필요가 없습니다.
+#if 0
 int main(int argc, char** argv)
 {
     testing::InitGoogleTest(&argc, argv);
 
     return RUN_ALL_TESTS();
 }
+#endif
