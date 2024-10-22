45e3f9042780d5bf0f1a8080a39a201e30a24684
Chansik Yun <chansigi@gmail.com>
2024-10-22 04:21:04 +0000

    .

--- 12_테스트리스너.cpp
+++ 12_테스트리스너.cpp
@@ -7,7 +7,7 @@
 TEST(SampleTest, foo) { }
 TEST(SampleTest, goo) { }
 
-TEST(ImageTest, foo) { }
+TEST(ImageTest, foo) { FAIL(); }
 
 TEST(ImageProcessorTest, foo) { }
 
@@ -24,6 +24,7 @@ public:
     {
         std::cout << "OnEnvironmentsSetUpStart" << std::endl;
     }
+
     void OnEnvironmentsSetUpEnd(const UnitTest& /*unit_test*/) override
     {
         std::cout << "OnEnvironmentsSetUpEnd" << std::endl;
@@ -50,6 +51,47 @@ public:
     }
 };
 
+using namespace testing;
+
+class TersePrinter : public EmptyTestEventListener {
+private:
+    // Called before any test activity starts.
+    void OnTestProgramStart(const UnitTest& /* unit_test */) override { }
+
+    // Called after all test activities have ended.
+    void OnTestProgramEnd(const UnitTest& unit_test) override
+    {
+        fprintf(stdout, "TEST %s\n", unit_test.Passed() ? "PASSED" : "FAILED");
+        fflush(stdout);
+    }
+
+    // Called before a test starts.
+    void OnTestStart(const TestInfo& test_info) override
+    {
+        fprintf(stdout, "*** Test %s.%s starting.\n", test_info.test_suite_name(),
+            test_info.name());
+        fflush(stdout);
+    }
+
+    // Called after a failed assertion or a SUCCEED() invocation.
+    void OnTestPartResult(const TestPartResult& test_part_result) override
+    {
+        fprintf(stdout, "%s in %s:%d\n%s\n",
+            test_part_result.failed() ? "*** Failure" : "Success",
+            test_part_result.file_name(), test_part_result.line_number(),
+            test_part_result.summary());
+        fflush(stdout);
+    }
+
+    // Called after a test ends.
+    void OnTestEnd(const TestInfo& test_info) override
+    {
+        fprintf(stdout, "*** Test %s.%s ending.\n", test_info.test_suite_name(),
+            test_info.name());
+        fflush(stdout);
+    }
+}; // class TersePrinter
+
 // 2) 리스너 등록
 // => main 함수를 직접 정의해서 등록해야 합니다.
 int main(int argc, char** argv)
@@ -60,7 +102,11 @@ int main(int argc, char** argv)
     testing::TestEventListeners& listeners = testing::UnitTest::GetInstance()->listeners();
 
     // 2. 리스너를 등록합니다.
-    listeners.Append(new TestListener);
+    // listeners.Append(new TestListener);
+    listeners.Append(new TersePrinter);
+
+    // 기본 출력 프린터를 제거할 수 있습니다.
+    delete listeners.Release(listeners.default_result_printer());
 
     return RUN_ALL_TESTS();
 }
