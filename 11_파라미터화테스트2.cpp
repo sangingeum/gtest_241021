f5303b39933a7e813ba6ce79f6f250447555e1f1
Chansik Yun <chansigi@gmail.com>
2024-10-22 02:06:51 +0000

    .

--- 11_파라미터화테스트2.cpp
+++ 11_파라미터화테스트2.cpp
@@ -13,3 +13,15 @@ bool IsPrime(int value)
 
 // 2, 3, 5, 7, 11, 13, 17, 23, 29 ...
 // 위의 데이터를 파라미터화 테스트를 통해 직접 작성해보세요.
+#include <gtest/gtest.h>
+
+class PrimeTest : public testing::TestWithParam<int> {
+};
+
+INSTANTIATE_TEST_SUITE_P(PrimeValues, PrimeTest,
+    testing::Values(2, 3, 5, 7, 11, 13, 17, 23, 29));
+
+TEST_P(PrimeTest, IsPrime)
+{
+    EXPECT_TRUE(IsPrime(GetParam()));
+}
---
+++ 11_파라미터화테스트3.cpp
@@ -0,0 +1,46 @@
+// 11_파라미터화테스트3.cpp
+
+bool IsPrime(int value)
+{
+    for (int i = 2; i < value; ++i) {
+        if (value % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+#include <gtest/gtest.h>
+
+// 입력 데이터가 여러개인 경우
+// 1) tuple
+using InputType = std::tuple<int, int>;
+
+class PrimeTest : public testing::TestWithParam<InputType> { };
+
+INSTANTIATE_TEST_SUITE_P(PrimeValues, PrimeTest,
+    testing::Values(
+        InputType { 2, 4 },
+        InputType { 3, 6 },
+        InputType { 5, 8 },
+        InputType { 7, 10 },
+        InputType { 11, 12 },
+        InputType { 13, 14 },
+        InputType { 17, 18 }));
+
+TEST_P(PrimeTest, IsPrime_Good)
+{
+    const InputType& data = GetParam();
+    int good = std::get<0>(data);
+
+    EXPECT_TRUE(IsPrime(good));
+}
+
+TEST_P(PrimeTest, IsPrime_Bad)
+{
+    const InputType& data = GetParam();
+    int bad = std::get<1>(data);
+
+    EXPECT_FALSE(IsPrime(bad));
+}
