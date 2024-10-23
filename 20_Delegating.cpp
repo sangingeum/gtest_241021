437742ccaaf77b6aed79097ce0644fe6fb538d51
Chansik Yun <chansigi@gmail.com>
2024-10-23 02:27:44 +0000

    .

--- 20_Delegating.cpp
+++ 20_Delegating.cpp
@@ -26,12 +26,26 @@ public:
     MOCK_METHOD(int, Sub, (int a, int b), (override));
 };
 
+// - MOCK_METHOD한 함수의 결과는 기본값을 반환합니다.
+// - Google Mock은 MOCK_METHOD를 수행한 메소드에 대해서,
+//   행위 기반 검증 뿐 아니라 결과를 제어할 수 있는 기능을 제공하고 있습니다.
+//  => Delegating(위임)
+//    : ON_CALL
+
+using testing::NiceMock;
+using testing::Return;
+
 TEST(CalcTest, Process)
 {
-    MockCalc mock;
+    NiceMock<MockCalc> mock;
+    ON_CALL(mock, Add(10, 20)).WillByDefault(Return(30));
 
-    EXPECT_CALL(mock, Add(10, 20));
-    EXPECT_CALL(mock, Sub(100, 50));
+    // EXPECT_CALL(mock, Add(10, 20));
+    // EXPECT_CALL(mock, Sub(100, 50));
 
-    Process(&mock);
+    std::cout << mock.Add(10, 20) << std::endl;
+    std::cout << mock.Add(10, 20) << std::endl;
+    std::cout << mock.Add(10, 20) << std::endl;
+
+    // Process(&mock);
 }
