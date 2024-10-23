8b8bcb427944772648d94e75f5c7491c9c1855d3
Chansik Yun <chansigi@gmail.com>
2024-10-23 01:44:45 +0000

    .

---
+++ 18_Mocking6.cpp
@@ -0,0 +1,47 @@
+// 18_Mocking6.cpp
+enum Level {
+    INFO,
+    WARN
+};
+
+class Logger {
+public:
+    virtual ~Logger() { }
+
+    virtual void Send(Level level, const char* dir, const char* file, const char* message) = 0;
+};
+
+class Car {
+public:
+    void Start(Logger* logger)
+    {
+        // ...
+        logger->Send(INFO, "/tmp2", "car.log", "car start...");
+        // ...
+    }
+};
+
+// Car 객체가 logger를 사용할 때, /tmp 디렉토리에 로그를 기록하는지 여부를 검증하고 싶습니다.
+//  => 모든 인자에 대한 검증이 아니라, 특정한 인자에 대해서만 검증하고 싶습니다.
+
+#include <gmock/gmock.h>
+// 방법 1. 목 간략화
+class MockLogger : public Logger {
+public:
+    void Send(Level level, const char* dir, const char* file, const char* message) override
+    {
+        Send(dir); // MOCK_METHOD한 메소드를 직접 호출해줍니다.
+    }
+
+    MOCK_METHOD(void, Send, (const char* dir)); // void Send(const char* dir)
+};
+
+TEST(CarTest, Start)
+{
+    MockLogger logger;
+    Car car;
+
+    EXPECT_CALL(logger, Send("/tmp"));
+
+    car.Start(&logger);
+}
